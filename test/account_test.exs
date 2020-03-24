8ffe3edfff2622232193e3d89a6fa716cf45a60d
Enitan Williams <akhanaton@gmail.com>
2020-03-24 11:24:20 +0000

    refactor account process test

--- test/account_test.exs
+++ test/account_test.exs
@@ -3,75 +3,36 @@ defmodule AccountTest do
 
   @subject Twitter.Core.Account
 
-  alias Twitter.Core.{ProcessRegistry, Tweet, TweetServer, User}
+  alias Twitter.Core.{Timeline, Tweet, User}
 
   setup do
-    {:ok, user1} = User.new("alice@fakemail.fake", "Alice Bryan", "alice")
+    {:ok, user} = User.new("alice@fakemail.fake", "Alice Bryan", "alice")
+    user = %{user | id: UUID.uuid1()}
+    tweet1 = Tweet.new("First tweet")
+    tweet2 = Tweet.new("Second tweet")
+    tweet1 = %{tweet1 | id: UUID.uuid1(), user_id: user.id}
+    tweet2 = %{tweet2 | id: UUID.uuid1(), user_id: user.id}
+    timeline = Timeline.new()
+    timeline = Timeline.add(timeline, tweet1)
+    timeline = Timeline.add(timeline, tweet2)
+    account = %{user: user, timeline: timeline}
 
-    # account_pid1 = AccountsSupervisor.account_process(user1)
-    {:ok, account_pid1} = start_supervised({Twitter.Core.Account, user1})
-
-    :timer.sleep(200)
-
-    via_log1 = Twitter.Core.TweetServer.via_tuple("alice")
-    tweet1 = Tweet.new("Hello world!")
-    TweetServer.tweet(user1, tweet1)
-    :timer.sleep(200)
-    %{timeline: timeline1, user: user1} = :sys.get_state(account_pid1)
-
-    on_exit(fn ->
-      GenServer.stop(via_log1, :normal)
-    end)
-
-    [
-      account_pid: account_pid1,
-      timeline: timeline1,
-      tweet: tweet1,
-      user: user1
-    ]
+    %{account: account}
   end
 
-  describe "tweets/2" do
-    # @tag :pending
-    test "retrieve tweets from users tweetlog", state do
-      tweet = state[:tweet]
-      user = state[:user]
-
-      [actual] = @subject.tweets(user)
-
-      assert %{content: tweet.content} == %{content: actual.content}
-    end
-
-    # @tag :pending
-    test "tweets are sorted from newest to oldest", state do
-      user = state[:user]
-
-      tweet2 = Tweet.new("Second tweet")
-      TweetServer.tweet(user, tweet2)
-
-      [actual | _rest] = @subject.tweets(user)
-
-      assert %{content: tweet2.content} == %{content: actual.content}
-    end
+  test "get user details", %{account: account} do
+    expected = account.user
+    assert {:reply, actual, _, _} = @subject.handle_call(:user, "caller", account)
+    assert ^expected = actual
   end
 
-  # @tag :pending
-  describe "toggle_follower/2" do
-    test "followed user should be in my following", state do
-      user = state[:user]
-      :timer.sleep(100)
-      {:ok, user2} = User.new("bob@fakemail.fake", "Bob C.", "bob")
-      {:ok, account_pid2} = @subject.start_link(user2)
-      :timer.sleep(100)
-      via_log = Twitter.Core.TweetServer.via_tuple("bob")
-      %{timeline: timeline, user: user2} = :sys.get_state(account_pid2)
+  test "add user id to following", %{account: account} do
+    {:ok, user} = User.new("bob@fakemail.fake", "Bob C.", "bob")
+    user = %{user | id: UUID.uuid1()}
 
-      actual = @subject.toggle_follower(user, user2)
+    assert {:reply, actual, _, _} =
+             @subject.handle_call({:toggle_following, user}, "caller", account)
 
-      assert user2.id in actual.followers
-
-      GenServer.stop(account_pid2, :normal)
-      GenServer.stop(via_log, :normal)
-    end
+    assert user.id in actual.following
   end
 end
