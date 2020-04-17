fde4291552c7023e65ab60d5ff212c8fc1bb18ae
Enitan Williams <akhanaton@gmail.com>
2020-04-17 19:03:27 +0100

    add remove followers

--- lib/twitter/account/account.ex
+++ lib/twitter/account/account.ex
@@ -1,4 +1,5 @@
 defmodule Twitter.Core.Account do
+  import Ecto.Query
   alias Twitter.Core.{Account.User, Repo}
 
   def create_user(user_details) do
@@ -6,6 +7,11 @@ defmodule Twitter.Core.Account do
     |> Map.put(:display_name, "@#{user_details.username}")
     |> build_user
     |> Repo.insert()
+  end
+
+  def delete_follower(%{followed_id: followed_id, follower_id: follower_id}) do
+    query = from("followers", where: [user_id: ^followed_id, follower_id: ^follower_id])
+    Repo.delete_all(query)
   end
 
   def get_user_by_credentials(%{"email" => email, "password" => pass}) do
@@ -20,8 +26,14 @@ defmodule Twitter.Core.Account do
     end
   end
 
-  # Private
+  def insert_follower(%{user_id: user_id, follower_id: follower_id}) do
+    user = Repo.get(User, user_id)
+    follower = Ecto.build_assoc(user, :followers, follower_id: follower_id)
 
+    Repo.insert(follower)
+  end
+
+  # Private
   defp get_user_by_email(email), do: Repo.get_by(User, email: email)
 
   defp build_user(user_details) do
